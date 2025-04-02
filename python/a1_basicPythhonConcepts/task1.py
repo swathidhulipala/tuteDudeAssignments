de307c080365fc69d677678ff72e50f5d4403a21
SwathiDhulipala <swathi.dhulipala1@gmail.com>
2025-04-02 18:54:53 +0530

    git config --global user.email "swathi.dhulipala1@gmail.com" user.name "Swathi Dhulipala"

---
+++ python/a1_basicPythhonConcepts/task1.py
@@ -0,0 +1,6 @@
+var1=input("Enter the first number ")
+var2=input("Enter the second number ")
+print("Addition: ",int(var1)+int(var2))
+print("Subtraction: ",int(var1)-int(var2))
+print("Multiplication: ",int(var1)*int(var2))
+print("Division: ",int(var1)/int(var2))
---
+++ python/a1_basicPythhonConcepts/task2.py
@@ -0,0 +1,3 @@
+var1=input("Enter the first name: ")
+var2=input("Enter the last name: ")
+print("Hello", var1,var2+"! Welcome to the python program.")
